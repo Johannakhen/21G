1af8bb0da6cacfa306127840119bfebda18c5b75
Johanna Khenkittisak <johanna.khen@gmail.com>
2019-06-11 21:54:53 +0200

    update to vue

---
+++ 21g/src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <section>
+    <div class="split split__left" :class="{open : leftPanel, close: rightPanel}">
+        <div class="centered">
+            <h2>Le concept</h2>
+            <div v-if="!rightPanel">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <button class="button" v-on:click="openLeft">Découvrir</button>
+            </div>
+        </div>
+    </div>
+    <div class="split split__right" :class="{open : rightPanel, close: leftPanel}">
+      <div class="centered">
+          <h2>L'histoire</h2>
+          <div v-if="!leftPanel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button class="button" v-on:click="openRight">Découvrir</button>
+          </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      leftPanel : false,
+      rightPanel : false,
+    }
+  },
+  methods: {
+    openLeft() {
+      return this.leftPanel = true
+    },
+    openRight() {
+      return this.rightPanel = true
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.split {
+  height: 100%;
+  width: 50%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+.split__left {
+  left: 0;
+  background-color: #111;
+}
+.split__right {
+  right: 0;
+  background-color: red;
+}
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.split.open {
+  width: 90%;
+  transition: 0.5s width ease-in-out;
+}
+.split.close {
+  width: 10%;
+  transition: 0.5s width ease-in-out;
+}
+.close h2 { transform: rotate(90deg); }
+</style>

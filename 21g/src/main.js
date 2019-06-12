e0c8a0698f6dbca7c6e9216f8675ec9da4c3c45b
Johanna Khenkittisak <johanna.khen@gmail.com>
2019-06-12 18:27:34 +0200

    Change order of reset css

--- 21g/src/main.js
+++ 21g/src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import './assets/css/reset.css'
 import './assets/css/font.css'
 import './assets/css/global.css'
+import App from './App'
+import router from './router'
 
 Vue.config.productionTip = false
 
